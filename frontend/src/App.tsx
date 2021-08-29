a6b684b6f69802d65c43f3b7b9aff2e76e19a8f5
Leonardo Batista Carias <leonardo1carias@gmail.com>
2021-08-29 15:55:46 -0300

    Creating single page application navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,21 @@
 import { Box } from '@material-ui/core';
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Breadcrumbs from './components/Breadcrumbs';
 import { Navbar } from './components/Navbar';
-import { Page } from './components/Page';
+import AppRouter from './routes/AppRouter';
 
 const App: React.FC = () => {
   return (
-    <React.Fragment>    
-      <Navbar/>
-      <Box paddingTop={'70px'}>
-        <Page title={'Categorias'}/>
-      </Box>      
+    <React.Fragment>
+      <BrowserRouter>
+        <Navbar/>
+        <Box paddingTop={'70px'}>
+          <Breadcrumbs />
+          <AppRouter />
+        </Box>
+      </BrowserRouter>         
     </React.Fragment>
   );
 }
--- frontend/src/components/Navbar/Menu.tsx
+++ frontend/src/components/Navbar/Menu.tsx
@@ -1,10 +1,17 @@
 import { IconButton, Menu as MuiMenu, MenuItem } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import * as React from 'react';
-type Props = {
+import { Link } from 'react-router-dom';
+import routes, {MyRouteProps} from '../../routes';
+
+const listRoutes = [
+    'dashboard',
+    'categories.list'
+];
+const menuRoutes = routes.filter(route => listRoutes.includes(route.name));
+
+export const Menu = () => {
     
-};
-export const Menu = (props: Props) => {
     const [anchorEl, setAnchorEl] = React.useState(null);
     const open = Boolean(anchorEl);
     
@@ -32,10 +39,20 @@ export const Menu = (props: Props) => {
                 transformOrigin={{vertical: 'top', horizontal: 'center'}}
                 getContentAnchorEl={null}
             >
-                <MenuItem onClick={handleClose}>
-                    Categorias
-                </MenuItem>
-                
+                {
+                    listRoutes.map(
+                        (routeName, key) => {
+                            const route = menuRoutes.find(
+                                route => route.name === routeName
+                            ) as MyRouteProps;                            
+                            return (
+                                <MenuItem key={key} component={Link} to={route.path as string} onClick={handleClose}>
+                                    {route.label}
+                                </MenuItem>
+                            )           
+                        }
+                    )
+                }
             </MuiMenu>
         </React.Fragment>
     );
